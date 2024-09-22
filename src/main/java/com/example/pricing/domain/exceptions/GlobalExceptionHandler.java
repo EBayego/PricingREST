3085c398383bcf94bfae1ad8167e607d3821fce4
EBayego <edubayego@gmail.com>
2024-09-22 09:56:34 +0200

    HotFix: Fixed Test & Global Exception
    Fixed the test, changing the approach so it works better.
    
    Fixed the GlobalExceptionHandler, as before it didn't return the right response.

--- src/main/java/com/example/pricing/domain/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/pricing/domain/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.example.pricing.domain.exceptions;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
@@ -11,33 +8,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
 
-import reactor.core.publisher.Mono;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PriceNotFoundException.class)
-    public Mono<ResponseEntity<Map<String, Object>>> handlePriceNotFoundException(PriceNotFoundException ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", LocalDateTime.now());
-        response.put("status", HttpStatus.NOT_FOUND.value());
-        response.put("error", "Not Found");
-        response.put("message", ex.getMessage());
-
-        return Mono.just(new ResponseEntity<>(response, HttpStatus.NOT_FOUND));
+    public ResponseEntity<String> handlePriceNotFoundException(PriceNotFoundException ex) {
+    	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(WebExchangeBindException.class)
-    public Mono<ResponseEntity<String>> handleMissingParams(WebExchangeBindException ex) {
+    public ResponseEntity<String> handleMissingParams(WebExchangeBindException ex) {
         String errorMessage = "Hay errores en la solicitud: " + ex.getFieldErrors().stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
-        return Mono.just(new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST));
+    	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 
     @ExceptionHandler(Exception.class)
-    public Mono<ResponseEntity<String>> handleGeneralException(Exception ex) {
-        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Ocurrió un error inesperado: " + ex.getMessage()));
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Ocurrió un error inesperado: " + ex.getMessage());
     }
 }
--- src/test/java/com/store/order/PriceControllerIntegrationTest.java
+++ src/test/java/com/store/order/PriceControllerIntegrationTest.java
@@ -1,107 +1,199 @@
 package com.store.order;
 
 import com.example.pricing.PricingApplication;
+import com.example.pricing.application.service.PriceService;
 import com.example.pricing.domain.model.Price;
 import com.example.pricing.domain.repository.PriceRepository;
+import com.example.pricing.infrastructure.mapper.PriceMapper;
+
+import reactor.core.publisher.Mono;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = PricingApplication.class)
 @AutoConfigureMockMvc
+@AutoConfigureWebTestClient
 class PriceControllerIntegrationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@MockBean
+    private PriceService priceService;
+
+	@MockBean
+    private PriceMapper priceMapper;
 
     @Autowired
     private PriceRepository priceRepository;
+
+    @Autowired
+    private WebTestClient webTestClient;
+    
+    private static List<Price> prices;
+    
+    @BeforeAll
+    public static void setup() {
+    	prices = List.of(
+                new Price(null, 1L, LocalDateTime.of(2020, 6, 14, 0, 0), LocalDateTime.of(2020, 12, 31, 23, 59), 1, 35455L, 0, 35.50, "EUR"),
+                new Price(null, 1L, LocalDateTime.of(2020, 6, 14, 15, 0), LocalDateTime.of(2020, 6, 14, 18, 30), 2, 35455L, 1, 25.45, "EUR"),
+                new Price(null, 1L, LocalDateTime.of(2020, 6, 15, 0, 0), LocalDateTime.of(2020, 6, 15, 11, 0), 3, 35455L, 1, 30.50, "EUR"),
+                new Price(null, 1L, LocalDateTime.of(2020, 6, 15, 16, 0), LocalDateTime.of(2020, 12, 31, 23, 59), 4, 35455L, 1, 38.95, "EUR")
+            );
+    }
 
     @BeforeEach
     void setUp() {
         // Limpiar la base de datos antes de cada prueba
         priceRepository.deleteAll();
-
-        List<Price> prices = List.of(
-            new Price(null, 1L, LocalDateTime.of(2020, 6, 14, 0, 0), LocalDateTime.of(2020, 12, 31, 23, 59), 1, 35455L, 0, 35.50, "EUR"),
-            new Price(null, 1L, LocalDateTime.of(2020, 6, 14, 15, 0), LocalDateTime.of(2020, 6, 14, 18, 30), 2, 35455L, 1, 25.45, "EUR"),
-            new Price(null, 1L, LocalDateTime.of(2020, 6, 15, 0, 0), LocalDateTime.of(2020, 6, 15, 11, 0), 3, 35455L, 1, 30.50, "EUR"),
-            new Price(null, 1L, LocalDateTime.of(2020, 6, 15, 16, 0), LocalDateTime.of(2020, 12, 31, 23, 59), 4, 35455L, 1, 38.95, "EUR")
-        );
         priceRepository.saveAll(prices);
     }
 
     @Test
     void test1_getPrice_at1000_on14th() throws Exception {
-        mockMvc.perform(get("/api/prices/find")
-                .param("productId", "35455")
-                .param("brandId", "1")
-                .param("date", "2020-06-14T10:00:00"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.price").value(35.50))  // Tarifa con priceList 1
-                .andExpect(jsonPath("$.priceList").value(1));
-    }
+    Long productId = 35455L;
+    Long brandId = 1L;
+    String applicationDate = "2020-06-14T10:00:00";
+
+    Price price = prices.get(0);
+
+    when(priceService.getApplicablePrice(productId, brandId, LocalDateTime.parse(applicationDate)))
+        .thenReturn(Mono.just(price));
+
+    webTestClient.get()
+        .uri(uriBuilder -> uriBuilder
+            .path("/api/prices/find")
+            .queryParam("productId", productId)
+            .queryParam("brandId", brandId)
+            .queryParam("date", applicationDate)
+            .build())
+        .exchange()
+        .expectStatus().isOk()  // Verificar que la respuesta es 200 OK
+        .expectBody()
+        .jsonPath("$.priceList").isEqualTo(1)
+        .jsonPath("$.price").isEqualTo(35.50);
+}
 
     @Test
     void test2_getPrice_at1600_on14th() throws Exception {
-        mockMvc.perform(get("/api/prices/find")
-                .param("productId", "35455")
-                .param("brandId", "1")
-                .param("date", "2020-06-14T16:00:00"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.price").value(25.45))  // Tarifa con priceList 2
-                .andExpect(jsonPath("$.priceList").value(2));
+        Long productId = 35455L;
+        Long brandId = 1L;
+        String applicationDate = "2020-06-14T16:00:00";
+
+        Price price = prices.get(1);
+
+        when(priceService.getApplicablePrice(productId, brandId, LocalDateTime.parse(applicationDate)))
+            .thenReturn(Mono.just(price));
+
+        webTestClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/api/prices/find")
+                .queryParam("productId", productId)
+                .queryParam("brandId", brandId)
+                .queryParam("date", applicationDate)
+                .build())
+            .exchange()
+            .expectStatus().isOk() 
+            .expectBody()
+            .jsonPath("$.priceList").isEqualTo(2)
+            .jsonPath("$.price").isEqualTo(25.45);
     }
 
     @Test
     void test3_getPrice_at2100_on14th() throws Exception {
-        mockMvc.perform(get("/api/prices/find")
-                .param("productId", "35455")
-                .param("brandId", "1")
-                .param("date", "2020-06-14T21:00:00"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.price").value(35.50))  // Tarifa con priceList 1
-                .andExpect(jsonPath("$.priceList").value(1));
+    	Long productId = 35455L;
+        Long brandId = 1L;
+        String applicationDate = "2020-06-14T21:00:00";
+
+        Price price = prices.get(2);
+
+        when(priceService.getApplicablePrice(productId, brandId, LocalDateTime.parse(applicationDate)))
+            .thenReturn(Mono.just(price));
+
+        webTestClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/api/prices/find")
+                .queryParam("productId", productId)
+                .queryParam("brandId", brandId)
+                .queryParam("date", applicationDate)
+                .build())
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+            .jsonPath("$.priceList").isEqualTo(1)
+            .jsonPath("$.price").isEqualTo(35.50);
     }
 
     @Test
     void test4_getPrice_at1000_on15th() throws Exception {
-        mockMvc.perform(get("/api/prices/find")
-                .param("productId", "35455")
-                .param("brandId", "1")
-                .param("date", "2020-06-15T10:00:00"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.price").value(30.50))  // Tarifa con priceList 3
-                .andExpect(jsonPath("$.priceList").value(3));
+    	Long productId = 35455L;
+        Long brandId = 1L;
+        String applicationDate = "2020-06-15T10:00:00";
+
+        Price price = prices.get(3);
+
+        when(priceService.getApplicablePrice(productId, brandId, LocalDateTime.parse(applicationDate)))
+            .thenReturn(Mono.just(price));
+
+        webTestClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/api/prices/find")
+                .queryParam("productId", productId)
+                .queryParam("brandId", brandId)
+                .queryParam("date", applicationDate)
+                .build())
+            .exchange()
+            .expectStatus().isOk() 
+            .expectBody()
+            .jsonPath("$.priceList").isEqualTo(3)
+            .jsonPath("$.price").isEqualTo(30.50);
     }
 
     @Test
     void test5_getPrice_at2100_on16th() throws Exception {
-        mockMvc.perform(get("/api/prices/find")
-                .param("productId", "35455")
-                .param("brandId", "1")
-                .param("date", "2020-06-16T21:00:00"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.price").value(38.95))  // Tarifa con priceList 4
-                .andExpect(jsonPath("$.priceList").value(4));
+    	Long productId = 35455L;
+        Long brandId = 1L;
+        String applicationDate = "2020-06-16T21:00:00";
+
+        Price price = prices.get(2);
+
+        when(priceService.getApplicablePrice(productId, brandId, LocalDateTime.parse(applicationDate)))
+            .thenReturn(Mono.just(price));
+
+        webTestClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/api/prices/find")
+                .queryParam("productId", productId)
+                .queryParam("brandId", brandId)
+                .queryParam("date", applicationDate)
+                .build())
+            .exchange()
+            .expectStatus().isOk() 
+            .expectBody()
+            .jsonPath("$.priceList").isEqualTo(4)
+            .jsonPath("$.price").isEqualTo(38.95);
     }
 
     @Test
     void getPrice_shouldReturn404_whenPriceNotFound() throws Exception {
-        mockMvc.perform(get("/api/prices/find")
-                .param("productId", "99999")
-                .param("brandId", "1")
-                .param("date", "2020-06-14T00:00:00"))
-                .andExpect(status().isNotFound());
+    	webTestClient.get()
+        .uri(uriBuilder -> uriBuilder
+            .path("/api/prices/find")
+            .queryParam("productId", "99999")
+            .queryParam("brandId", "1")
+            .queryParam("date", "2020-06-14T00:00:00")
+            .build())
+        .exchange()
+        .expectStatus().isNotFound();
     }
 }
